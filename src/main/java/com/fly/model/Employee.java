1a17549d4fafdc44b89036d820ea5da17ecc81b8
amirtharaj vellingiri <amirtharaj@live.ccom>
2021-06-11 11:11:35 +0530

    Filter Def and Filter annotation

--- src/main/java/com/fly/model/Employee.java
+++ src/main/java/com/fly/model/Employee.java
@@ -3,13 +3,20 @@ package com.fly.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.*;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.persistence.*;
 
 @Entity
 @Setter
 @NoArgsConstructor
 @Getter
+@SQLDelete(sql = "UPDATE employee SET deleted = true WHERE id = ?")
+//@Where(clause = "deleted = false") Exclude this to retrieve all deleted data
+@FilterDef(name = "deleteEmployeeFilter", parameters = @ParamDef(name = "isDeleted", type = "boolean"))
+@Filter(name = "deleteEmployeeFilter", condition = "deleted = :isDeleted")
 @Table(uniqueConstraints = @UniqueConstraint(name = "UniqueNameAndEmail", columnNames = {"name"}))
 public class Employee {
 
@@ -23,6 +30,8 @@ public class Employee {
 
     @OneToOne
     private Account account;
+
+    private boolean deleted = false;
 
     public Employee(String name) {
         this.name = name;
--- src/main/java/com/fly/service/EmployeeService.java
+++ src/main/java/com/fly/service/EmployeeService.java
@@ -3,8 +3,13 @@ package com.fly.service;
 import com.fly.model.Employee;
 import com.fly.repository.AccountRepository;
 import com.fly.repository.EmployeeRepository;
+import org.hibernate.Filter;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import java.util.Set;
 
 @Service
 public class EmployeeService {
@@ -14,6 +19,9 @@ public class EmployeeService {
 
     @Autowired
     private AccountRepository accountRepository;
+
+    @Autowired
+    private EntityManager entityManager;
 
     public void lets(String name) {
 
@@ -27,8 +35,17 @@ public class EmployeeService {
     }
 
     public void second(Long id) {
-        Employee employee = employeeRepository.findById(id).get();
-        System.out.println("Employee is " + employee.getAccount());
+
+        //Employee employee = employeeRepository.findById(id).get();
+        //employeeRepository.delete(employee);
+
+        //System.out.println("Employee is " + employee.getAccount());
+
+        Session session = entityManager.unwrap(Session.class);
+        Filter filter = session.enableFilter("deleteEmployeeFilter");
+        filter.setParameter("isDeleted", true);
+        Set<Employee> employeeList = employeeRepository.findAll();
+        System.out.println(employeeList);
     }
 
 }
